143ca462a73b8c68a8ea5954a9840952538ee52b
Thomas Tumiel <ttumiel@gmail.com>
2018-10-19 00:38:10 +0200

    Add a pretty snackbar

---
+++ client/src/components/SnackbarMessage.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+
+import snackbarStyle from "../assets/js/snackbarStyle";
+
+const variantIcon = {
+    success: CheckCircleIcon,
+    warning: WarningIcon,
+    error: ErrorIcon,
+    info: InfoIcon,
+};
+
+class SnackbarMessage extends Component {
+    state = {
+        open: true,
+    };
+
+    handleClick = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+    }
+
+        this.setState({ open: false });
+    };
+
+  render() {
+    const { classes, className, message, onClose, variant, ...other } = this.props;
+    const Icon = variantIcon[variant];
+
+    return (
+        <Snackbar
+            anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+            }}
+            open={this.state.open}
+            autoHideDuration={6000}
+            onClose={this.handleClose}
+        >
+            <SnackbarContent
+                className={classNames(classes[variant], className)}
+                aria-describedby="client-snackbar"
+                message={
+                    <span id="client-snackbar" className={classes.message}>
+                    <Icon className={classNames(classes.icon, classes.iconVariant)} />
+                    {message}
+                    </span>
+                }
+                action={[
+                    <IconButton
+                    key="close"
+                    aria-label="Close"
+                    color="inherit"
+                    className={classes.close}
+                    onClick={this.handleClose}
+                    >
+                    <CloseIcon className={classes.icon} />
+                    </IconButton>,
+                ]}
+                {...other}
+            />
+        </Snackbar>
+      );
+  }
+  
+}
+
+SnackbarMessage.propTypes = {
+    classes: PropTypes.object.isRequired,
+    className: PropTypes.string,
+    message: PropTypes.node,
+    onClose: PropTypes.func,
+    variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
+  };
+
+export default withStyles(snackbarStyle)(SnackbarMessage);
