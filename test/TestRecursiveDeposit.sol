70c2de8d5186c100a871052fd254788c75f36145
Thomas Tumiel <ttumiel@gmail.com>
2018-11-30 21:41:03 +0200

    Add solidity contract test

---
+++ test/TestRecursiveDeposit.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.24;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/RecursiveDeposit.sol";
+
+contract TestRecursiveDeposit {
+    RecursiveDeposit rd = RecursiveDeposit(DeployedAddresses.RecursiveDeposit());
+
+    address owner = this;
+
+    function testGetTotalPot() public {
+        uint pot = rd.getTotalPot();
+
+        Assert.equal(pot, 0, "Initial pot is 0.");
+    }
+}
