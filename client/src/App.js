cc993235e11c447c1f4fcbeaf3fa71af34d83d73
Thomas Tumiel <ttumiel@gmail.com>
2019-01-01 17:36:37 +0200

    format document

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,46 @@
 import React, { Component } from "react";
 
 // Material Ui Core
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 // Material Ui Styles
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from "@material-ui/core/styles";
 
 // Material Ui Icons
-import Code from '@material-ui/icons/Code';
+import Code from "@material-ui/icons/Code";
 
 // Ethereum
 // import RecursiveDepositContract from "./contracts/RecursiveDeposit.json";
 import RecursiveDepositABI from "./RecursiveDeposit.js";
 import getWeb3 from "./utils/getWeb3";
 // import truffleContract from "truffle-contract";
-import {BigNumber} from 'bignumber.js';
+import { BigNumber } from "bignumber.js";
 
 // Other components
-import SnackbarMessage from "./components/SnackbarMessage"
+import SnackbarMessage from "./components/SnackbarMessage";
 
 import "./App.css";
 import styles from "./assets/js/appStyle";
 
 class App extends Component {
   // isLoading disables the button when querying the contract
-  state = { web3: null, accounts: null, contract: null, isLoading: false, totalPot: 0, newBets: [], betTimeDiff: 30 };
+  state = {
+    web3: null,
+    accounts: null,
+    contract: null,
+    isLoading: false,
+    totalPot: 0,
+    newBets: [],
+    betTimeDiff: 30
+  };
 
   componentDidMount = async () => {
     try {
@@ -50,15 +57,23 @@ class App extends Component {
 
       // contract address on ropsten: "0x6be39b681ce4dbb1866602b0d90011d3a01a6b67"
       // web3 v1: https://web3js.readthedocs.io/en/1.0/index.html
-      var instance = await new web3.eth.Contract(RecursiveDepositABI, "0x933dccab2d7fe84b6522e1ab210f8621bb3ac3ab");
+      var instance = await new web3.eth.Contract(
+        RecursiveDepositABI,
+        "0x933dccab2d7fe84b6522e1ab210f8621bb3ac3ab"
+      );
 
       // console.log(instance);
-      let currentPot = BigNumber((await instance.methods.getTotalPot().call({from: accounts[0]})).toString()).dividedBy(BigNumber('1e18'));
+      let currentPot = BigNumber(
+        (await instance.methods
+          .getTotalPot()
+          .call({ from: accounts[0] })).toString()
+      ).dividedBy(BigNumber("1e18"));
       let lastTime = await instance.methods.getLastBetTime().call();
 
       const timeDelay = 300 * 60; // 300 minutes
       let timeNow = new Date();
-      let timeDiff = timeDelay - (Math.floor(timeNow.getTime()/1000) - lastTime);
+      let timeDiff =
+        timeDelay - (Math.floor(timeNow.getTime() / 1000) - lastTime);
       // console.log(currentPot);
       // .then((tp) => {
       //  currentPot = BigNumber(tp.toString()).dividedBy(BigNumber('1e18'));
@@ -66,7 +81,13 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance, totalPot: currentPot, betTimeDiff: timeDiff });
+      this.setState({
+        web3,
+        accounts,
+        contract: instance,
+        totalPot: currentPot,
+        betTimeDiff: timeDiff
+      });
       setTimeout(this.watchPot, 1000);
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -79,9 +100,10 @@ class App extends Component {
     const prevState = this.state;
 
     let updatedBets = [];
-    contract.events.NewBet({fromBlock: 0, toBlock: 'latest'},
+    contract.events.NewBet(
+      { fromBlock: 0, toBlock: "latest" },
       (error, event) => {
-        if (error){
+        if (error) {
           console.log(error);
         }
         console.log(event.returnValues);
@@ -90,9 +112,9 @@ class App extends Component {
         this.setState({
           newBets: this.state.newBets.concat([event.returnValues])
         });
-      });
-      // console.log(...updatedBets);
-
+      }
+    );
+    // console.log(...updatedBets);
 
     console.log(this.state.newBets);
     // console.log(event);
@@ -110,14 +132,17 @@ class App extends Component {
   watchMyBets = async () => {
     const { accounts, contract } = this.state;
 
-    contract.NewBet({_addr: accounts[0]}, {fromBlock: 0, toBlock: 'latest'})
-    .watch(function(err, result) {
-      if (err) {
-        console.log(err)
-        return;
-      }
-      console.log("New bet by " + result.args._addr + " of " + result.args._value);
-    })
+    contract
+      .NewBet({ _addr: accounts[0] }, { fromBlock: 0, toBlock: "latest" })
+      .watch(function(err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(
+          "New bet by " + result.args._addr + " of " + result.args._value
+        );
+      });
 
     // depositEvent.stopWatching()
   };
@@ -126,13 +151,13 @@ class App extends Component {
     const { accounts, contract, totalPot } = this.state;
 
     // Check the pot
-    let tp = await contract._totalPot( { from: accounts[0] });
+    let tp = await contract._totalPot({ from: accounts[0] });
     console.log(tp);
 
     // Update state with the result.
     this.setState({
       totalPot: tp
-    })
+    });
   };
 
   makeBet = async () => {
@@ -140,9 +165,11 @@ class App extends Component {
 
     // Check the pot
     try {
-      let tp = await contract.methods.bet(accounts[0]).send({ from: accounts[0], value: "5000000000000000" });
+      let tp = await contract.methods
+        .bet(accounts[0])
+        .send({ from: accounts[0], value: "5000000000000000" });
       console.log(tp);
-    } catch (error){
+    } catch (error) {
       // return (
       //   <SnackbarMessage
       //     variant="error"
@@ -163,90 +190,114 @@ class App extends Component {
     // }
     return (
       <div className={classes.layout}>
-      <Grid container item md={12} alignItems="center" justify="space-between" className={classes.toolbarMain}>
-        <Grid item>
-        <Typography
-          component="h2"
-          variant="h4"
-          color="inherit"
-          className={classes.toolbarTitle}
+        <Grid
+          container
+          item
+          md={12}
+          alignItems="center"
+          justify="space-between"
+          className={classes.toolbarMain}
         >
-          Recursive Bet
-        </Typography>
-        </Grid>
-        <Grid item>
-        <IconButton className={classes.button} aria-label="GitHub" href="https://github.com/Tom2718">
-          <Code />
-        </IconButton>
-        </Grid>
-      </Grid>
-
-
-      <main>
-        <Grid container spacing={40} className={classes.mainGrid} alignItems="center">
-          <Grid item md={4} align="center" >
-            <h4>{totalPot.toString()}</h4>
-            <p>ETH in the Pot</p>
-          </Grid>
-          <Grid item md={4} align="center">
-          <h2>Win Now!</h2>
-          <p>
-            Only {Math.floor(betTimeDiff/60)} minutes to go.
-          </p>
-          </Grid>
-          <Grid item md={4} align="center" >
-            <Button
-              classes={{
-                root: classes.playButton,
-              }}
-              disabled={isLoading}
-              onClick={!isLoading ? this.makeBet : null}
+          <Grid item>
+            <Typography
+              component="h2"
+              variant="h4"
+              color="inherit"
+              className={classes.toolbarTitle}
             >
-              {isLoading ? 'Loading...' : 'Bet now!'}
-            </Button>
-          </Grid>
-        </Grid>
-        <Divider className={classes.divider} />
-        <Grid container spacing={40} justify="center" className={classes.mainGrid}>
-          <Grid item md={6} className={classes.mainFeaturedPostContent}>
-          The aim of the game is to be the last person to put ETH into the pot - if you remain there for 30 minutes then you win the entire pot instantly.
-          Ok - the real aim is to practice coding and thinking about smart contracts and developing (secure) DApps. Unless if you make millions - then share!
-          <Divider className={classes.divider} />
-
-            <Typography variant="h6" className={classes.title}>
-              Previous Bets
+              Recursive Bet
             </Typography>
-            <div className={classes.demo}>
-              <List>
-                {newBets.map( (bet) =>
-                  <ListItem key={bet._addr}>
-                    <ListItemIcon>
-                      <Code />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={bet._addr.toString()}
-                      secondary={BigNumber(bet._value.toString()).dividedBy(BigNumber('1e18')).toString() + " ETH"}
-                    />
-                  </ListItem>,
-                )}
-              </List>
-            </div>
+          </Grid>
+          <Grid item>
+            <IconButton
+              className={classes.button}
+              aria-label="GitHub"
+              href="https://github.com/Tom2718"
+            >
+              <Code />
+            </IconButton>
           </Grid>
         </Grid>
-        {!this.state.web3 ?
-        <SnackbarMessage
-          variant="error"
-          className={classes.margin}
-          message="Error: Make sure Metamask is unlocked and connected to the correct Ethereum network."
-        />
-        :
-        <SnackbarMessage
-          variant="success"
-          className={classes.margin}
-          message="Success: Connected to the Ethereum network."
-        />
-         }
-      </main>
+
+        <main>
+          <Grid
+            container
+            spacing={40}
+            className={classes.mainGrid}
+            alignItems="center"
+          >
+            <Grid item md={4} align="center">
+              <h4>{totalPot.toString()}</h4>
+              <p>ETH in the Pot</p>
+            </Grid>
+            <Grid item md={4} align="center">
+              <h2>Win Now!</h2>
+              <p>Only {Math.floor(betTimeDiff / 60)} minutes to go.</p>
+            </Grid>
+            <Grid item md={4} align="center">
+              <Button
+                classes={{
+                  root: classes.playButton
+                }}
+                disabled={isLoading}
+                onClick={!isLoading ? this.makeBet : null}
+              >
+                {isLoading ? "Loading..." : "Bet now!"}
+              </Button>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <Grid
+            container
+            spacing={40}
+            justify="center"
+            className={classes.mainGrid}
+          >
+            <Grid item md={6} className={classes.mainFeaturedPostContent}>
+              The aim of the game is to be the last person to put ETH into the
+              pot - if you remain there for 30 minutes then you win the entire
+              pot instantly. Ok - the real aim is to practice coding and
+              thinking about smart contracts and developing (secure) DApps.
+              Unless if you make millions - then share!
+              <Divider className={classes.divider} />
+              <Typography variant="h6" className={classes.title}>
+                Previous Bets
+              </Typography>
+              <div className={classes.demo}>
+                <List>
+                  {newBets.map(bet => (
+                    <ListItem key={bet._addr}>
+                      <ListItemIcon>
+                        <Code />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={bet._addr.toString()}
+                        secondary={
+                          BigNumber(bet._value.toString())
+                            .dividedBy(BigNumber("1e18"))
+                            .toString() + " ETH"
+                        }
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              </div>
+            </Grid>
+          </Grid>
+          {!this.state.web3 ? (
+            <SnackbarMessage
+              variant="error"
+              className={classes.margin}
+              message="Error: Make sure Metamask is unlocked and connected to the correct Ethereum network."
+            />
+          ) : (
+            <SnackbarMessage
+              variant="success"
+              className={classes.margin}
+              message="Success: Connected to the Ethereum network."
+            />
+          )}
+        </main>
       </div>
     );
   }
--- client/src/assets/js/appStyle.js
+++ client/src/assets/js/appStyle.js
@@ -1,58 +1,58 @@
 const styles = theme => ({
-    layout: {
-      width: 'auto',
-      marginLeft: theme.spacing.unit * 3,
-      marginRight: theme.spacing.unit * 3,
-      [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-        width: 1100,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
-    },
-    toolbarMain: {
-      borderBottom: `1px solid ${theme.palette.grey[300]}`,
-      maxWidth: 1300,
-    },
-    toolbarTitle: {
-      flex: 1,
-    },
-    mainFeaturedPost: {
-      backgroundColor: theme.palette.grey[800],
-      color: theme.palette.common.white,
-      marginBottom: theme.spacing.unit * 4,
-    },
-    mainFeaturedPostContent: {
-      padding: `${theme.spacing.unit * 4}px`,
-      [theme.breakpoints.up('md')]: {
-        paddingRight: 0,
-      },
-    },
-    mainGrid: {
-      marginTop: theme.spacing.unit * 3,
-    },
-    divider: {
-      margin: `${theme.spacing.unit * 3}px 0`,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      marginTop: theme.spacing.unit * 8,
-      padding: `${theme.spacing.unit * 6}px 0`,
-    },
-    button: {
-      margin: theme.spacing.unit,
-    },
-    margin: {
-      margin: theme.spacing.unit,
-    },
-    playButton: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 3,
-      border: 0,
-      color: 'white',
-      height: 48,
-      padding: '0 45px',
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    },
-  });
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  toolbarMain: {
+    borderBottom: `1px solid ${theme.palette.grey[300]}`,
+    maxWidth: 1300
+  },
+  toolbarTitle: {
+    flex: 1
+  },
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 4}px`,
+    [theme.breakpoints.up("md")]: {
+      paddingRight: 0
+    }
+  },
+  mainGrid: {
+    marginTop: theme.spacing.unit * 3
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 3}px 0`
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    marginTop: theme.spacing.unit * 8,
+    padding: `${theme.spacing.unit * 6}px 0`
+  },
+  button: {
+    margin: theme.spacing.unit
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  playButton: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    borderRadius: 3,
+    border: 0,
+    color: "white",
+    height: 48,
+    padding: "0 45px",
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
+  }
+});
 
-  export default styles;
+export default styles;
